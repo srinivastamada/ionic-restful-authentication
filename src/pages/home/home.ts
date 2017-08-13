5713aa69e7bf65c8144742cfaa1270de6399990a
Srinivas Tamada <srinivas@9lessons.info>
2017-08-13 19:01:03 -0400

    change

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,59 +1,85 @@
-import { Component } from '@angular/core';
-import { NavController, App } from 'ionic-angular';
+
+import {Component} from '@angular/core';
+import {NavController, App} from 'ionic-angular';
 import {AuthService} from "../../providers/auth-service";
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
+@Component({selector: 'page-home', templateUrl: 'home.html'})
 export class HomePage {
   public userDetails : any;
-  public resposeData: any;
+  public resposeData : any;
   public dataSet : any;
-  userPostData = {"user_id":"", "token":""};
+  userPostData = {
+    "user_id": "",
+    "token": "",
+    "feed":""
+  };
 
-  constructor(public navCtrl: NavController, public app: App, public authService: AuthService) {
-   const data = JSON.parse(localStorage.getItem('userData'));
-   this.userDetails = data.userData;
-   this.userPostData.user_id = this.userDetails.user_id;
-   this.userPostData.token = this.userDetails.token;
+  constructor(public navCtrl : NavController, public app : App, public authService : AuthService) {
+    const data = JSON.parse(localStorage.getItem('userData'));
+    this.userDetails = data.userData;
+    this.userPostData.user_id = this.userDetails.user_id;
+    this.userPostData.token = this.userDetails.token;
 
-   this.getFeed();
+    this.getFeed();
 
   }
 
+  getFeed() {
+    this
+      .authService
+      .postData(this.userPostData, "feed")
+      .then((result) => {
+        this.resposeData = result;
+        if (this.resposeData.feedData) {
+          this.dataSet = this.resposeData.feedData;
+          console.log(this.dataSet);
+        } else {
+          console.log("No access");
+        }
 
-  getFeed(){
-   this.authService.postData(this.userPostData, "feed").then((result) =>{
-    this.resposeData = result;
-    if(this.resposeData.feedData){
-    this.dataSet = this.resposeData.feedData;
-    console.log(this.dataSet);
+      }, (err) => {
+        //Connection failed message
+      });
   }
-  else{
-    console.log("No access");
-  }
-    
+
+  feedUpdate() {
+    if(this.userPostData.feed){
    
-    }, (err) => {
-      //Connection failed message
-    });
+    this
+      .authService
+      .postData(this.userPostData, "feedUpdate")
+      .then((result) => {
+        this.resposeData = result;
+        if (this.resposeData.feedData) {
+          this.dataSet.unshift(this.resposeData.feedData);
+          this.userPostData.feed ="";
+        } else {
+          console.log("No access");
+        }
+
+      }, (err) => {
+        //Connection failed message
+      });
+    }
+    
   }
 
-  converTime(time){
-  let a = new Date(time*1000);
-  return a;
+  converTime(time) {
+    let a = new Date(time * 1000);
+    return a;
   }
 
-  backToWelcome(){
-   const root = this.app.getRootNav();
-   root.popToRoot();
+  backToWelcome() {
+    const root = this
+      .app
+      .getRootNav();
+    root.popToRoot();
   }
 
-  logout(){
-    //Api Token Logout 
+  logout() {
+    //Api Token Logout
     localStorage.clear();
-     setTimeout(()=> this.backToWelcome(), 1000);
+    setTimeout(() => this.backToWelcome(), 1000);
   }
 
 }
