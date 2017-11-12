b8e314281ec5313d9983dc28c970239681062a6c
Srinivas Tamada <srinivas@9lessons.info>
2017-11-12 17:35:08 -0500

    change

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,185 +1,185 @@
-import {Component, ViewChild} from '@angular/core';
-import {NavController, App, AlertController} from 'ionic-angular';
-import {AuthService} from "../../providers/auth-service";
-import {Common} from "../../providers/common";
+import { Component, ViewChild } from "@angular/core";
+import { NavController, App, AlertController } from "ionic-angular";
+import { AuthService } from "../../providers/auth-service";
+import { Common } from "../../providers/common";
 
-@Component({selector: 'page-home', templateUrl: 'home.html'})
+@Component({ selector: "page-home", templateUrl: "home.html" })
 export class HomePage {
-  @ViewChild('updatebox') updatebox;
-  public userDetails : any;
-  public resposeData : any;
-  public dataSet : any;
+  @ViewChild("updatebox") updatebox;
+  public userDetails: any;
+  public resposeData: any;
+  public dataSet: any;
+  public noRecords: boolean;
   userPostData = {
-    "user_id": "",
-    "token": "",
-    "feed": "",
-    "feed_id":"",
-    "lastCreated": "",
+    user_id: "",
+    token: "",
+    feed: "",
+    feed_id: "",
+    lastCreated: ""
   };
 
-  constructor(public common: Common, private alertCtrl: AlertController,public navCtrl : NavController, public app : App, public authService : AuthService) {
-    const data = JSON.parse(localStorage.getItem('userData'));
+  constructor(
+    public common: Common,
+    private alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public app: App,
+    public authService: AuthService
+  ) {
+    const data = JSON.parse(localStorage.getItem("userData"));
     this.userDetails = data.userData;
     this.userPostData.user_id = this.userDetails.user_id;
     this.userPostData.token = this.userDetails.token;
     this.userPostData.lastCreated = "";
+    this.noRecords = false'
     this.getFeed();
-
   }
 
   getFeed() {
     this.common.presentLoading();
-    this
-      .authService
-      .postData(this.userPostData, "feed")
-      .then((result) => {
+    this.authService.postData(this.userPostData, "feed").then(
+      result => {
         this.resposeData = result;
         if (this.resposeData.feedData) {
-              this.common.closeLoading();
+          this.common.closeLoading();
           this.dataSet = this.resposeData.feedData;
           console.log(this.dataSet);
 
           const dataLength = this.resposeData.feedData.length;
-         
-          this.userPostData.lastCreated = this.resposeData.feedData[dataLength-1].created;
 
+          this.userPostData.lastCreated = this.resposeData.feedData[
+            dataLength - 1
+          ].created;
         } else {
           console.log("No access");
         }
-
-      }, (err) => {
+      },
+      err => {
         //Connection failed message
-      });
+      }
+    );
   }
 
   feedUpdate() {
     if (this.userPostData.feed) {
       this.common.presentLoading();
-       this
-      .authService
-      .postData(this.userPostData, "feedUpdate")
-      .then((result) => {
-        this.resposeData = result;
-        if (this.resposeData.feedData) {
-          this.common.closeLoading();
-          this.dataSet.unshift(this.resposeData.feedData);
-          this.userPostData.feed = "";
+      this.authService.postData(this.userPostData, "feedUpdate").then(
+        result => {
+          this.resposeData = result;
+          if (this.resposeData.feedData) {
+            this.common.closeLoading();
+            this.dataSet.unshift(this.resposeData.feedData);
+            this.userPostData.feed = "";
 
-         
-
-         //this.updatebox.setFocus();
-         setTimeout(() => {
-        //  this.updatebox.focus();
-        },150);
-        } else {
-          console.log("No access");
+            //this.updatebox.setFocus();
+            setTimeout(() => {
+              //  this.updatebox.focus();
+            }, 150);
+          } else {
+            console.log("No access");
+          }
+        },
+        err => {
+          //Connection failed message
         }
-
-      }, (err) => {
-        //Connection failed message
-      });
+      );
     }
-
   }
 
   feedDelete(feed_id, msgIndex) {
-  
     if (feed_id > 0) {
-
-
       let alert = this.alertCtrl.create({
-    title: 'Delete Feed',
-    message: 'Do you want to buy this feed?',
-    buttons: [
-      {
-        text: 'Cancel',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancel clicked');
-        }
-      },
-      {
-        text: 'Delete',
-        handler: () => {
-           this.userPostData.feed_id = feed_id;
-       this
-      .authService
-      .postData(this.userPostData, "feedDelete")
-      .then((result) => {
-        this.resposeData = result;
-        if (this.resposeData.success) {
-        this.dataSet.splice(msgIndex, 1);
-        } else {
-          console.log("No access");
-        }
-
-      }, (err) => {
-        //Connection failed message
+        title: "Delete Feed",
+        message: "Do you want to buy this feed?",
+        buttons: [
+          {
+            text: "Cancel",
+            role: "cancel",
+            handler: () => {
+              console.log("Cancel clicked");
+            }
+          },
+          {
+            text: "Delete",
+            handler: () => {
+              this.userPostData.feed_id = feed_id;
+              this.authService.postData(this.userPostData, "feedDelete").then(
+                result => {
+                  this.resposeData = result;
+                  if (this.resposeData.success) {
+                    this.dataSet.splice(msgIndex, 1);
+                  } else {
+                    console.log("No access");
+                  }
+                },
+                err => {
+                  //Connection failed message
+                }
+              );
+            }
+          }
+        ]
       });
-        }
-      }
-    ]
-  });
-  alert.present();
-
-
-     
+      alert.present();
     }
-
   }
 
-  
-
-  doInfinitex(e){
-   console.log("I am here");
-   setTimeout(() => {
-    this.authService.postData(this.userPostData, "feed").then((result) => {
-      this.resposeData = result;
-      if (this.resposeData.feedData.length) {
-        const newData = this.resposeData.feedData;
-        this.userPostData.lastCreated = this.resposeData.feedData[newData.length-1].created;
-      
-        for (let i = 0; i < newData.length; i++) {
-          this.dataSet.push(newData[i] );
-        }
-
-      } else {
-        console.log("No user updates");
-      }
-
-    }, (err) => {
-      //Connection failed message
-    });
-    console.log('Async operation has ended');
-    e.complete();
-  }, 500);
-  }
-
-  doInfinite(e): Promise<any> {
-    console.log('Begin async operation');
-
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        this.authService.postData(this.userPostData, "feed").then((result) => {
+  doInfinitex(e) {
+    console.log("I am here");
+    setTimeout(() => {
+      this.authService.postData(this.userPostData, "feed").then(
+        result => {
           this.resposeData = result;
           if (this.resposeData.feedData.length) {
             const newData = this.resposeData.feedData;
-            this.userPostData.lastCreated = this.resposeData.feedData[newData.length-1].created;
-          
+            this.userPostData.lastCreated = this.resposeData.feedData[
+              newData.length - 1
+            ].created;
+
             for (let i = 0; i < newData.length; i++) {
-              this.dataSet.push(newData[i] );
+              this.dataSet.push(newData[i]);
             }
-    
           } else {
             console.log("No user updates");
           }
-    
-        }, (err) => {
+        },
+        err => {
           //Connection failed message
-        });
+        }
+      );
+      console.log("Async operation has ended");
+      e.complete();
+    }, 500);
+  }
+
+  doInfinite(e): Promise<any> {
+    console.log("Begin async operation");
+
+    return new Promise(resolve => {
+      setTimeout(() => {
+        this.authService.postData(this.userPostData, "feed").then(
+          result => {
+            this.resposeData = result;
+            if (this.resposeData.feedData.length) {
+              const newData = this.resposeData.feedData;
+              this.userPostData.lastCreated = this.resposeData.feedData[
+                newData.length - 1
+              ].created;
+
+              for (let i = 0; i < newData.length; i++) {
+                this.dataSet.push(newData[i]);
+              }
+            } else {
+              this.noRecords = true;
+              console.log("No user updates");
+            }
+          },
+          err => {
+            //Connection failed message
+          }
+        );
         resolve();
       }, 500);
-    })
+    });
   }
 
   converTime(time) {
@@ -188,9 +188,7 @@ export class HomePage {
   }
 
   backToWelcome() {
-    const root = this
-      .app
-      .getRootNav();
+    const root = this.app.getRootNav();
     root.popToRoot();
   }
 
@@ -199,7 +197,5 @@ export class HomePage {
 
     localStorage.clear();
     setTimeout(() => this.backToWelcome(), 1000);
-
   }
-
 }
